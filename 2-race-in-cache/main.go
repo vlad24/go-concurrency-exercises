53560320b8583f724c5480e78378e48e10eb9e76
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2022-10-24 20:34:02 +0200

    Prorotype solution for 2

--- 2-race-in-cache/main.go
+++ 2-race-in-cache/main.go
@@ -8,7 +8,10 @@
 
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"sync"
+)
 
 // CacheSize determines how big the cache can grow
 const CacheSize = 100
@@ -29,6 +32,7 @@ type KeyStoreCache struct {
 	cache map[string]*list.Element
 	pages list.List
 	load  func(string) string
+	mutex *sync.RWMutex
 }
 
 // New creates a new KeyStoreCache
@@ -36,17 +40,22 @@ func New(load KeyStoreCacheLoader) *KeyStoreCache {
 	return &KeyStoreCache{
 		load:  load.Load,
 		cache: make(map[string]*list.Element),
+		mutex: &sync.RWMutex{},
 	}
 }
 
 // Get gets the key from cache, loads it from the source if needed
 func (k *KeyStoreCache) Get(key string) string {
+	k.mutex.RLock()
+	defer k.mutex.RUnlock()
 	if e, ok := k.cache[key]; ok {
 		k.pages.MoveToFront(e)
 		return e.Value.(page).Value
 	}
 	// Miss - load from database and save it in cache
 	p := page{key, k.load(key)}
+	k.mutex.Lock()
+	defer k.mutex.Unlock()
 	// if cache is full remove the least used item
 	if len(k.cache) >= CacheSize {
 		end := k.pages.Back()
